1230e12cb360bd3637bb51e51dbae58d69ebce65
GOSU <asus6972@gmail.com>
2023-08-24 09:40:02 +0300

    logging
    trying to log some info

--- cian.py
+++ cian.py
@@ -1,20 +1,23 @@
 import os
 import json
-import time
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",format="%(asctime)s %(levelname)s %(message)s")
+
 class Driver:
     """Создает и возвращает драйвер"""
 
     def __init__(self):
-        service = Service(ChromeDriverManager(
-            version="114.0.5735.90").install())
-        self.driver = webdriver.Chrome(service=service)
+        service = Service()
+        options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(service=service,options=options)
         self.driver.maximize_window()
+        logging.info("Driver init done")
 
     @property
     def get_driver(self) -> webdriver:
@@ -41,6 +44,7 @@ class Urls:
         urls = str_urls.split("\n")
         if "" in urls:
             urls.remove("")
+        logging.info("get_urls complete")
         return urls
 
     def write_url(self, url):
@@ -48,12 +52,14 @@ class Urls:
         with open("urls.ini", "a", encoding="utf-8") as file:
             file.write(url+"\n")
             file.close()
+        logging.info("write_url complete")
 
     def clear_urls(self):
         """ Очищает файл от ссылок """
         with open("urls.ini", "w", encoding="utf-8") as file:
             file.write("")
             file.close()
+        logging.info("clear_urls complete")
 
 
 class Parsing:
@@ -68,31 +74,32 @@ class Parsing:
 
     def get_source(self, url):
         """ Получение html страницы по ссылке """
+        logging.info(f"get_source url: {url}") 
         # Открытие ссылки
         try:
             urls = Urls()
             self.driver.get(url)
             self.url = str(self.driver.current_url)
         except Exception as exception:
-            print("Драйвер ", exception)
+            logging.exception(f"driver get url: {exception}")
             self.stop()
+        else:
+            logging.info(f"driver get_source done.")
         try:
             # Если парсер прошел все страницы
             if self.url in self.url_list:
                 urls.clear_urls()
-                print("Прошел")
+                logging.info(f"Parsing done. Closing parser.")
                 self.stop()
             else:
                 self.url_list = ur.get_urls
-                # Создание временного файла с данными
                 self.data = self.driver.page_source
-
         except Exception as exception:
-            print("Запись файла ", exception)
-
+            logging.exception(f"data exception: {exception}")         
         finally:
             # Обработка временного файла с данными
             self.get_items(self.page_number(self.url))
+            logging.info(f"get items complete")
 
     def get_items(self, page_number: int):
         """ Получение нужных элементов для записи в JSON файл.
@@ -100,12 +107,15 @@ class Parsing:
             
         """
         try:
+            logging.info(f"call get_items")
             # Рабта с BS4
             soup = BeautifulSoup(self.data, "lxml")
             # Ищем все блоки article
             item_divs = soup.find_all("article")
         except Exception as exception:
-            print("article ", exception)
+            logging.exception(f"Cant find arcticle: {exception}")
+        else:
+            logging.info("Article found")
         # Перебираем их для обработки
         data_list = []
         for item in item_divs:
@@ -115,45 +125,58 @@ class Parsing:
                 item_href = item.find(
                     "a", class_="_93444fe79c--media--9P6wN").get("href")
             except Exception as exception:
-                print("href ", exception)
+                logging.exception(f"Cant find href: {exception}")
+            else:
+                logging.info("href found")
             try:
                 # Получаем заголовок обьявления
                 header = item.find(
                     "span", attrs={"data-mark": "OfferTitle"}).find("span").text
             except Exception as exception:
-                print("Header ", exception)
+                logging.exception(f"Cant find header: {exception}")
+            else:
+                logging.info("header found")
             try:
                 # Адрес разделен на несколько блоков, ищем все
                 sep_names = item.find_all(
                     "a", class_="_93444fe79c--link--NQlVc")
             except Exception as exception:
-                print("sep_names ", exception)
+                logging.exception(f"Cant find sep_names: {exception}")
+            else:
+                logging.info("sep_names found")
             try:
                 # Создаем пустую строку в которую закидваем текст
                 addr = ""
                 for name in sep_names:
                     addr += f" {name.contents[0]}"
             except Exception as exception:
-                print("name_contents ", exception)
+                logging.exception(f"generating name: {exception}")
+            else:
+                logging.info("name done")
             try:
                 # Получаем цену предмета
                 price = item.find(
                     "span", attrs={"data-mark": "MainPrice"}).find("span").text
             except Exception as exception:
-                print("price ", exception)
+                logging.exception(f"Cant find price: {exception}")
+            else:
+                logging.info("price found")
             try:
                 # Получаем описание предмета (не знаю в чем проблема)
                 content = item.find(
                     "div", attrs={"data-name": "Description"}).find("p").text
             except Exception as exception:
-                print("content ", exception)
+                logging.exception(f"Cant find content: {exception}")
+            else:
+                logging.info("content found")
             try:
                 # Получаем застройщика
                 dev = item.find("div",
                                 class_="_93444fe79c--name-container--enElO").find("span").text
             except Exception as exception:
-                print("dev ", exception)
-
+                logging.exception(f"Cant find dev_name: {exception}")
+            else:
+                logging.info("dev_name found")
             try:
                 # Создаем словарь для записи в JSON
                 new_data = {
@@ -167,13 +190,17 @@ class Parsing:
                 }
                 data_list.append(new_data)
             except Exception as exception:
-                print("Dic", exception)
+                logging.exception(f"Cant asseble dic: {exception}")
+            else:
+                logging.info("dic done")        
         try:
             with open("file.json", "a", encoding="utf-8") as json_:
                 json.dump(data_list, json_, indent=5, ensure_ascii=False)
                 json_.close()
         except Exception as exception:
-            print("Запись Json", exception)
+            logging.exception(f"json write exception: {exception}")
+        else:
+            logging.info("json done")  
         try:
             # Записываем в файл что мы запарсили эту страницу
             ur.write_url(self.url)
@@ -181,9 +208,12 @@ class Parsing:
             #time.sleep(1)
             self.get_source(self.next_page(self.url))
         except Exception as exception:
-            print("next_page g_sourse", exception)
+            logging.exception(f"cant get_source from next_page: {exception}")
+        else:
+            logging.info(f"Page parsed {page_number}") 
 
     def page_number(self, url: str) -> int:
+        logging.info(f"call page_number") 
         """ Получает номер страницы
 
         Args:
@@ -197,6 +227,7 @@ class Parsing:
         return int(page)
 
     def next_page(self, url: str) -> str:
+        logging.info(f"call next_page") 
         """ Создает новую ссылку
 
         Args:
@@ -212,14 +243,17 @@ class Parsing:
 
     def stop(self):
         """ Выход из программы """
+        logging.info(f"call stop") 
         os._exit(1)
 
 
 if __name__ == "__main__":
+    logging.info(f"Parser start") 
     p = Parsing()
     ur = Urls()
     # Создает файл если его нет
     if not os.path.isfile("urls.ini"):
+        logging.info(f"clearing urls") 
         ur.clear_urls()
     # Если нет ссылок в ini файле, он начинает с новой, если есть то продолжает
     if len(ur.get_urls) == 0:
